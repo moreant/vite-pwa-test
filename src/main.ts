ce448dcc095ac6df4d50f4aca851b57a71b6f0fa
moreant <moreant@foxmail.com>
2022-01-24 02:41:42 +0800

    sw test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { registerSW } from "virtual:pwa-register";
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
+
 createApp(App).mount('#app')
